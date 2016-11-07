ca3bae0baa13896492fa1977cc092794491078bd
Raman Gupta <rocketraman@gmail.com>
2016-11-07 14:04:15 -0500

    Akka: remove non-routed implementation

--- akka/src/main/kotlin/org/kotlin/community/web/helloworld/akka/Akka.kt
+++ akka/src/main/kotlin/org/kotlin/community/web/helloworld/akka/Akka.kt
@@ -3,34 +3,36 @@ package org.kotlin.community.web.helloworld.akka
 import akka.actor.*
 import akka.http.javadsl.*
 import akka.http.javadsl.model.*
-import akka.japi.function.*
+import akka.http.javadsl.server.*
+import akka.http.javadsl.server.Directives.*
 import akka.stream.*
-import akka.stream.javadsl.*
 import org.kotlin.community.web.helloworld.common.*
-
-val NOT_FOUND = HttpResponse.create().withStatus(404).withEntity("Unknown resource!")
 
 fun main(args: Array<String>) {
   val system = ActorSystem.create()
   val materializer = ActorMaterializer.create(system)
 
-  val serverSource = Http.get(system).bind(ConnectHttp.toHost("localhost", 8080), materializer)
+  val routeFlow = Akka.createRoute().flow(system, materializer)
+  val binding = Http.get(system).bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), materializer)
+}
 
-  val requestHandler = Function<HttpRequest, HttpResponse> { request ->
-    val uri = request.uri
-    if (request.method() == HttpMethods.GET) {
-      when (uri.path()) {
-        "/" -> HttpResponse.create().withEntity(ContentTypes.TEXT_PLAIN_UTF8, HELLO_WORLD)
-        else -> NOT_FOUND
-      }
-    } else {
-      NOT_FOUND
+object Akka {
+  fun createRoute(): Route {
+    return get {
+      route(
+        // matches the empty path
+        pathSingleSlash {
+          // return a constant string with a certain content type
+          complete(HttpEntities.create(ContentTypes.TEXT_PLAIN_UTF8, HELLO_WORLD))
+        },
+        path("ping") {
+          // return a simple `text/plain` response
+          complete("PONG!")
+        },
+        path("image.png") {
+          getFromResource("public/image.png")
+        }
+      )
     }
   }
-
-  val serverBindingFuture = serverSource.to(Sink.foreach { connection ->
-    connection.handleWithSyncHandler(requestHandler, materializer)
-    // this is equivalent to
-    //connection.handleWith(Flow.of(HttpRequest.class).map(requestHandler), materializer);
-  }).run(materializer)
 }
