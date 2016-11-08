0161e8beb98b9e4d0c11c2e45090ae63097b27cf
Raman Gupta <rocketraman@gmail.com>
2016-11-08 14:31:28 -0500

    ktor: update to 0.2.3

--- ktor/src/main/kotlin/org/kotlin/community/web/helloworld/ktor/Ktor.kt
+++ ktor/src/main/kotlin/org/kotlin/community/web/helloworld/ktor/Ktor.kt
@@ -7,14 +7,12 @@ import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.routing.*
 import org.kotlin.community.web.helloworld.common.*
 
-class Ktor : ApplicationModule() {
-  override fun Application.install() {
-    install(DefaultHeaders)
-    install(Routing) {
-      get("/") {
-        call.respondText(HELLO_WORLD)
-      }
-      serveClasspathResources("public")
+fun Application.main() {
+  install(DefaultHeaders)
+  install(Routing) {
+    get("/") {
+      call.respondText(HELLO_WORLD)
     }
+    serveClasspathResources("public")
   }
 }
